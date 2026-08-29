cf9a61eaf33bbc886db03f2c181ded77242a0421
Luís Miguel <vilanovamiguel1@gmail.com>
2026-08-29 16:39:28 -0300

    feat: criacao do pacote spring

---
+++ src/main/java/com/mobdata/pontocerto/PontocertoApplication.java
@@ -0,0 +1,13 @@
+package com.mobdata.pontocerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontocertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontocertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobdata/pontocerto/PontocertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobdata.pontocerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontocertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
