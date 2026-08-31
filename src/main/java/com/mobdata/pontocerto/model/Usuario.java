f416a816811548724a74901cabf5c1a2b0d06455
Luís Miguel <vilanovamiguel1@gmail.com>
2026-08-31 16:54:54 -0300

    feat: criacao do model Usuario.java

---
+++ src/main/java/com/mobdata/pontocerto/model/Usuario.java
@@ -0,0 +1,41 @@
+package com.mobdata.pontocerto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_usuario", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "usuario")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "usuario", nullable = false, unique = true)
+    private String usuario;
+
+    @Column(name = "senha_hash", nullable = false)
+    private String senhaHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "perfil", nullable = false)
+    private Perfil perfil;
+    // Superadmin não pertence a nenhuma empresa específica (enxerga todas)
+    @ManyToOne
+    @JoinColumn(name = "empresa_id", nullable = true)
+    private Empresa empresa;
+}
