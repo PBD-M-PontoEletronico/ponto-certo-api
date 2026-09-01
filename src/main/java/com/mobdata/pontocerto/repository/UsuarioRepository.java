a8a31ebc6e46d117d51421262828a5a687379cc9
Luís Miguel <vilanovamiguel1@gmail.com>
2026-09-01 13:54:20 -0300

    feat: repository das entidades

---
+++ src/main/java/com/mobdata/pontocerto/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.mobdata.pontocerto.repository;
+
+import com.mobdata.pontocerto.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByUsuario(String usuario);
+}
